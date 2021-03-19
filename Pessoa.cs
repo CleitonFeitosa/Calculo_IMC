1b8a37529e72343eac426da3abfd2f5a7b288bf3
Cleiton Feitosa <cleiton.costa@outlook.com.br>
2021-03-19 09:27:50 -0300

    Calculadora de IMC

---
+++ Pessoa.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Pessoa
+{
+    public double peso, altura;
+
+    public double imccalc()
+    {
+        return (peso / (altura * altura));
+    }
+    public string verifimc(double imccalc)
+    {
+        if (imccalc < 18.5)
+        {
+            return ("Você esta Abaixo do Peso");
+        }
+        else if (imccalc < 25)
+        {
+            return ("Você está com o Peso Normal");
+        }
+        else if (imccalc < 30)
+        {
+            return ("Você esta Acima do Peso");
+        }
+        else if (imccalc <35)
+        {
+            return ("Você esta em Obesidade I");
+        }
+        else if (imccalc < 40)
+        {
+            return ("Você esta em Obesidade II");
+        }
+        else
+        {
+            return ("Você esta em Obesidade III");
+        }
+
+    }
+    public void mensagem()
+    {
+        double obterimc = imccalc();
+
+        string obtersituação = verifimc(obterimc);
+
+        Console.WriteLine("O seu IMC é: " + obterimc + "e " + obtersituação);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resposta = "sim";
+
+            while (resposta == "sim")
+            {
+                Pessoa a = new Pessoa();
+                Console.WriteLine("Por favor digite seu peso");
+                a.peso = double.Parse(Console.ReadLine());
+                Console.WriteLine("Por favor digite sua altura");
+                a.altura = double.Parse(Console.ReadLine());
+                a.mensagem();
+                do
+                {
+                    Console.WriteLine("Gostaria de Calcular novamente? Digite sim ou não");
+                    resposta = Console.ReadLine();
+                    resposta = resposta.Trim();
+                    resposta = resposta.ToLower();
+                    if (resposta == "sim")
+                    {
+                        resposta = "sim";
+                    }
+                    else if (resposta == "não")
+                    {
+                        resposta = "não";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Resposta invalida!");
+                        resposta = "erro";
+                    }
+                }
+                while (resposta == "erro");
+                Console.Clear();
+            }
+        }
+    }
+}
